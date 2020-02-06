7d2b6a4858b62ebeb5217db31ec2b898ee8a48e0
Saša Živkov <sasa.zivkov@sap.com>
2020-02-06 14:14:16 +0100

    Rename package to metricsreporterjmx
    Both metrics-reporter-jmx and metrics-reporter-prometheus plugins used
    the same package name: com.googlesource.gerrit.plugins.metricsreporters
    Adding a class with the same name in both plugins would be no problem
    for the bazel build but it is a problem for the generated eclipse
    project. Eclipse reports an error because of a type defined multiple
    times.
    
    Change-Id: I01866284ded303550f39b5239a5dd7d2b34d6f6c

--- src/main/java/com/googlesource/gerrit/plugins/metricsreporterjmx/GerritJmxReporter.java
+++ src/main/java/com/googlesource/gerrit/plugins/metricsreporterjmx/GerritJmxReporter.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.googlesource.gerrit.plugins.metricsreporters;
+package com.googlesource.gerrit.plugins.metricsreporterjmx;
 
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.jmx.JmxReporter;
